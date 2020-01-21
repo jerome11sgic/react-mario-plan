a1aa6097681e808fa98c2a068719f34e4a925a40
jerome11sgic <jerome11sgic@gmail.com>
2020-01-21 12:20:02 +0530

    redux and dummy data added

---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,13 @@
+const initState={
+    projects:[
+        {id:'1',title:'help me find peach',content:'ahh ahh blah blah'},
+        {id:'2',title:'collect all the stars',content:'ahh ahh blah blah'},
+        {id:'3',title:'egg hunt with yoshi',content:'ahh ahh blah blah'},
+    ]
+}
+
+const projectReducer=(state=initState,action)=>{
+    return state;
+}
+
+export default projectReducer
